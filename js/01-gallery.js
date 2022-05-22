a289364a7891fb37b38520b0ad9b49bdb8ed6cef
MykolaHrytsenko <niksandler@gmail.com>
2022-05-22 16:02:13 +0300

    task 2 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,7 +4,7 @@ const imgGalleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createImgGalleryMarkup(galleryItems);
 
 imgGalleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
-imgGalleryContainer.addEventListener('click', onGalleryContainerClick)
+imgGalleryContainer.addEventListener('click', onGalleryContainerClick);
 
 
 function createImgGalleryMarkup(galleryItems) {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imgGalleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgGalleryMarkup(galleryItems);
+
+imgGalleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+// imgGalleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createImgGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__items">
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>        
+        </li>`;
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 600, animationSpeed: 600, fadeSpeed: 600, overlayOpacity: 0.5 });
