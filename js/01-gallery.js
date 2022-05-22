c864324d99d69febe7bd96d5b5d78289e899c03d
MykolaHrytsenko <niksandler@gmail.com>
2022-05-22 13:31:17 +0300

    add 1st task (not finished)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imgGalleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImgGalleryMarkup(galleryItems);
+
+imgGalleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+imgGalleryContainer.addEventListener('click', onGalleryContainerClick)
+
+
+function createImgGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>`;
+    }).join('');
+};
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    const isNotIMG = evt.target.classList.contains('gallery__image');
+
+    if (!isNotIMG) {
+        return
+    }
+    else {
+        const instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" width="800" height="600">`);
+        instance.show()
+    };
+
+}
